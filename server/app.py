338bc25bd7908ad40ec44e36e9a8b93091f4e45a
Silva-NK <kiagonsuzanne@gmail.com>
2025-06-26 00:57:54 +0300

    Added GET all, GET one and POST resources.

--- server/app.py
+++ server/app.py
@@ -17,11 +17,55 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            plants_list,
+            200,
+        )
+
+        return response
+    
+    def post(self):
+        data = request.get_json()
+
+        name = data.get("name")
+        image  = data.get("image")
+        price = data.get("price")
+
+        if not all([name, image, price]):
+            return make_response({"error": "Missing required fields."}, 400)
+        
+        new_plant = Plant(name=name, image=image, price=price)
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            new_plant.to_dict(),
+            201
+        )
+
+        return response
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(
+            plant_dict,
+            200
+        )
+
+        return response
         
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
